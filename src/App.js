dea3d0b9d08a1f546d4070a7dde1a293de63d853
Matheus-Oliveira-Chaves <Matheus.oliveira.chaves@hotmail.com>
2024-03-21 23:27:02 -0400

    update3

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import { Box } from "@mui/material";
+
 import "./App.css";
 import Home from "./views/Home";
 import ExerciseDetail from "./views/ExerciseDetail";
@@ -10,9 +11,16 @@ import ExerciseList from "./views/ExerciseList";
 import MyPage from "./views/MyPage";
 import Recipes from "./views/Recipes";
 import RegisterSignIn from "./views/RegisterSignIn";
-import MyRecipeFav from "./views/MyRecipeFav"; // Import MyRecipeFav component
 
-const app = () => {
+const App = () => {
+  const [exercises, setExercises] = useState([]);
+
+  // Function to add exercise to workout plan
+  const onAddToWorkoutPlan = (exercise) => {
+    console.log("hello");
+    setExercises([...exercises, exercise]);
+  };
+  console.log("exercises", exercises);
   return (
     <Box width="400px" sx={{ width: { xl: "1488px" } }} m="auto">
       <Navbar />
@@ -29,11 +37,10 @@ const app = () => {
         <Route path="/login" element={<RegisterSignIn />} />
         <Route path="/mypage" element={<MyPage exercises={exercises} />} />
         <Route path="/recipes" element={<Recipes />} />
-        {/* <Route path="/recipes/:id" element={<RecipeDetail />} /> */}
       </Routes>
       <Footer />
     </Box>
   );
 };
 
-export default app;
+export default App;
