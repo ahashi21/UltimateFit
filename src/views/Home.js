d8604e11827603099494cce8c8e7d2715f38ece0
Jerome Garcia <jeromeo.garcia@gmail.com>
2024-03-23 23:43:28 -0600

    adding homepage video

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Box, Link } from "@mui/material";
 import { useNavigate } from "react-router-dom";
+import video1 from "../assets/video1.mp4";
 
 const Home = () => {
   const navigate = useNavigate();
@@ -16,26 +17,33 @@ const Home = () => {
   return (
     <Box
       display="flex"
+      flexDirection="column"
       justifyContent="center"
       alignItems="center"
       height="100vh"
     >
-      <Link style={{ marginRight: "10px" }} onClick={handleExerciseClick}>
-        <img
-          src="/exercise_photo.jpg" // Path to your exercise photo
-          alt="Exercises"
-          style={{ width: "70%", maxWidth: "400px", cursor: "pointer" }}
-          title="Exercises"
-        />
-      </Link>
-      <Link style={{ marginLeft: "10px" }} onClick={handleRecipeClick}>
-        <img
-          src="/recipe_photo.jpg" // Path to your recipe photo
-          alt="Recipes"
-          style={{ width: "70%", maxWidth: "400px", cursor: "pointer" }}
-          title="Recipes"
-        />
-      </Link>
+      <video autoPlay muted loop style={{ width: "100%", marginTop: "-50px" }}>
+        <source src={video1} type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+      <Box mt={-80}>
+        <Link style={{ marginRight: "10px" }} onClick={handleExerciseClick}>
+          <img
+            src="/exercise_photo.jpg" // Path to your exercise photo
+            alt="Exercises"
+            style={{ width: "70%", maxWidth: "400px", cursor: "pointer" }}
+            title="Exercises"
+          />
+        </Link>
+        <Link style={{ marginLeft: "10px" }} onClick={handleRecipeClick}>
+          <img
+            src="/recipe_photo.jpg" // Path to your recipe photo
+            alt="Recipes"
+            style={{ width: "70%", maxWidth: "400px", cursor: "pointer" }}
+            title="Recipes"
+          />
+        </Link>
+      </Box>
     </Box>
   );
 };
