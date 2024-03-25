65ee96628bedee130bc471086d5aff9886256eeb
Jerome Garcia <jeromeo.garcia@gmail.com>
2024-03-25 10:19:39 -0600

    Fixing the git error

--- src/views/Home.js
+++ src/views/Home.js
@@ -29,36 +29,44 @@ const Home = () => {
         Your browser does not support the video tag.
       </video>
 
-      <Box className="fixed-buttons-container">
-        <Button
-          variant="contained"
-          sx={{
-            bgcolor: '#12AF57',
-            color: '#fff',
-            marginRight: "10px", // Add margin to the right
-            "&:hover": {
-              bgcolor: '#fff',
-              color: '#12AF57'
-            }
-          }}
-          onClick={handleExerciseClick}
-        >
-          Exercises
-        </Button>
-        <Button
-          variant="contained"
-          sx={{
-            bgcolor: '#12AF57',
-            color: '#fff',
-            "&:hover": {
-              bgcolor: '#fff',
-              color: '#12AF57'
-            }
-          }}
-          onClick={handleRecipeClick}
-        >
-          Recipes
-        </Button>
+      <Box className="content-container">
+        <Box className="about-us-container">
+          <h2>About Us</h2>
+          <p>Welcome to UltimateFit - your ultimate fitness companion! Whether you're just starting your fitness journey or aiming for specific fitness goals, UltimateFit is here to help you every step of the way. With personalized workout plans, comprehensive nutrition guidance, and a supportive community, achieving your fitness aspirations has never been easier.</p>
+          <p>Our slogan: "Empowering you to reach your ultimate fitness goals."</p>
+        </Box>
+
+        <Box className="fixed-buttons-container">
+          <Button
+            variant="contained"
+            sx={{
+              bgcolor: '#12AF57',
+              color: '#fff',
+              marginRight: "10px",
+              "&:hover": {
+                bgcolor: '#fff',
+                color: '#12AF57'
+              }
+            }}
+            onClick={handleExerciseClick}
+          >
+            Exercises
+          </Button>
+          <Button
+            variant="contained"
+            sx={{
+              bgcolor: '#12AF57',
+              color: '#fff',
+              "&:hover": {
+                bgcolor: '#fff',
+                color: '#12AF57'
+              }
+            }}
+            onClick={handleRecipeClick}
+          >
+            Recipes
+          </Button>
+        </Box>
       </Box>
     </Box>
   );
